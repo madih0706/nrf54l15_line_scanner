7a2aa840231e7bdee5672d03bdcf559ee3a6f570
madih0706 <madih@seas.upenn.edu>
2025-06-28 17:09:54 -0400

    sampling from adc working

--- src/main.c
+++ src/main.c
@@ -4,34 +4,19 @@
 */
 
 #include <zephyr/kernel.h>
-#include <zephyr/sys/printk.h>
 #include <zephyr/device.h>
-#include <zephyr/drivers/adc.h>
 #include <zephyr/devicetree.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/drivers/adc.h>
 
 static const struct adc_dt_spec adc_channel = ADC_DT_SPEC_GET(DT_PATH(zephyr_user));
+LOG_MODULE_REGISTER(adc_testing, LOG_LEVEL_DBG);
 
 int main(void)
 {
-        //validate that the ADC peripheral is ready before setting it up
-        if (!adc_is_ready_dt(&adc_channel)) {
-                printk("ADC controller devivce %s not ready\n", adc_channel.dev->name);
-                return 0;
-        } else {
-                printk("ADC controller devivce %s is ready\n", adc_channel.dev->name);
-        }
-
-        //set up ADC channel
-        int err = adc_channel_setup_dt(&adc_channel);
-        if (err < 0) {
-                printk("Could not setup channel #%d (%d)\n", 0, err);
-                return 0;
-        } else {
-                printk("Set up channel\n");
-        }
-
-        // define a variable of type struct adc_sequence and a buffer of type int16_t to 
-        // specify where the samples are to be written
+        int err;
+        uint32_t count = 0;
+        /* STEP 4.1 - Define a variable of type adc_sequence and a buffer of type uint16_t */
         int16_t buf;
         struct adc_sequence sequence = {
                 .buffer = &buf,
@@ -40,32 +25,43 @@ int main(void)
                 //Optional
                 //.calibrate = true,
         };
-
-        // intialize adc sequence
+        /* STEP 3.3 - validate that the ADC peripheral (SAADC) is ready */
+        if (!adc_is_ready_dt(&adc_channel)) {
+                LOG_ERR("ADC controller devivce %s not ready", adc_channel.dev->name);
+                return 0;
+        }
+        /* STEP 3.4 - Setup the ADC channel */
+        err = adc_channel_setup_dt(&adc_channel);
+        if (err < 0) {
+                LOG_ERR("Could not setup channel #%d (%d)", 0, err);
+                return 0;
+        }
+        /* STEP 4.2 - Initialize the ADC sequence */
         err = adc_sequence_init_dt(&adc_channel, &sequence);
         if (err < 0) {
-                printk("Could not initalize sequnce");
+                LOG_ERR("Could not initalize sequnce");
                 return 0;
-        } else {
-                printk("Intitialized sequence");
         }
-
-        int16_t val_mv;
         while (1) {
-                //read a sample from the adc channel
+                int val_mv;
+                /* STEP 5 - Read a sample from the ADC */
                 err = adc_read(adc_channel.dev, &sequence);
                 if (err < 0) {
-                        printk("Could not read (%d)", err);
+                        LOG_ERR("Could not read (%d)", err);
                         continue;
                 }
-
+                val_mv = (int)buf;
+                LOG_INF("ADC reading[%u]: %s, channel %d: Raw: %d", count++, adc_channel.dev->name,
+                        adc_channel.channel_id, val_mv);
+                /* STEP 6 - Convert raw value to mV*/
                 err = adc_raw_to_millivolts_dt(&adc_channel, &val_mv);
                 /* conversion to mV may not be supported, skip if not */
                 if (err < 0) {
-                        printk(" (value in mV not available)\n");
+                        LOG_WRN(" (value in mV not available)\n");
                 } else {
-                        printk(" = %d mV", val_mv);
+                        LOG_INF(" = %d mV", val_mv);
                 }
+                k_sleep(K_MSEC(1000));
         }
-
+        return 0;
 }
